499ab2f56be9d086749a6c8d3833c11103e5b883
pawelzak <pawel.zakieta@nask.pl>
2024-07-17 14:57:30 +0200

    intro detector working

--- intro_detector/detector.py
+++ intro_detector/detector.py
@@ -1,26 +1,59 @@
+import os.path
+import pickle
+
 import cv2
 import plotly.express as px
 import numpy as np
 import pandas as pd
 import hashlib
 
-def cache(fn):
-    def cached(*args, **kwargs):
+def cache(persistent=True):
+    CACHE_DIR = 'cache'
+
+    def get_key(args, kwargs):
         kwargs_keys = sorted(kwargs.keys())
         kwargs_vals = [kwargs[k] for k in kwargs_keys]
-        key = (args, tuple(kwargs_vals))
-        if key not in cached.cache:
-            print(f'first time running with args {args} and kwargs {kwargs}')
-            val = fn(*args, **kwargs)
-            cached.cache[key] = val
-        else:
-            print(f'using cache for args {args} and kwargs {kwargs}')
-            val = cached.cache[key]
-        return cached.cache[key]
-    cached.cache = {}
-    return cached
+        return (args, tuple(kwargs_vals))
 
-@cache
+    def get_cache_file(args, kwargs):
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        key = get_key(args, kwargs)
+        return os.path.join(CACHE_DIR, hashlib.md5(str(key).encode()).hexdigest() + '.pickle')
+
+    def get_cached(cache_dict, args, kwargs):
+        key = get_key(args, kwargs)
+        if key in cache_dict:
+            return cache_dict[key]
+        if persistent:
+            cache_file = get_cache_file(args, kwargs)
+            if os.path.exists(cache_file):
+                with open(cache_file, 'rb') as f:
+                    return pickle.load(f)
+        return None
+
+    def save_cache(cache_dict, args, kwargs, val):
+        key = get_key(args, kwargs)
+        cache_dict[key] = val
+        if persistent:
+            with open(get_cache_file(args, kwargs), 'wb') as f:
+                pickle.dump(val, f)
+
+    def decorator(fn):
+        def cached(*args, **kwargs):
+            val = get_cached(cached.cache, args, kwargs)
+            if val is None:
+                print(f'first time running with args {args} and kwargs {kwargs}')
+                val = fn(*args, **kwargs)
+                save_cache(cached.cache, args, kwargs, val)
+            else:
+                print(f'using cache for args {args} and kwargs {kwargs}')
+            return val
+        cached.cache = {}
+        return cached
+    return decorator
+
+
+@cache(persistent=True)
 def read_frames(path: str, max_frames: int = 5000, resize=(100,100)):
     stream = cv2.VideoCapture(path)
     frames = [cv2.resize(stream.read()[1].astype(float)/255, resize) for i in range(max_frames)]
@@ -40,8 +73,96 @@ def hash_image(image, res=8):
     image = image > image.mean()
     return hashlib.md5(image).hexdigest()
 
-paths = ['../static/The Office/season 4/The Office (US) (2005) - S04E01-E02 - Fun Run (1080p BluRay x265 Silence).mkv',
-         '../static/The Office/season 4/The Office (US) (2005) - S04E03-E04 - Dunder Mifflin Infinity (1080p BluRay x265 Silence).mkv']
-frames = read_frames(paths[0])
-frames2 = read_frames(paths[0])
-pass
+
+def dilate(a: np.array, size: int = 1):
+    kernel_size = 2 * size + 1
+    ii = np.arange(a.shape[0]) + (np.arange(kernel_size) - size)[:, None]
+    ii[ii<0] = 0
+    ii[ii>=a.shape[0]] = a.shape[0] - 1
+    return a[ii].any(axis=0)
+
+
+def erode(a: np.array, size: int = 1):
+    kernel_size = 2 * size + 1
+    ii = np.arange(a.shape[0]) + (np.arange(kernel_size) - size)[:, None]
+    ii[ii<0] = 0
+    ii[ii>=a.shape[0]] = a.shape[0] - 1
+    return a[ii].all(axis=0)
+
+def save_video(data: list[np.array], path: str):
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+    for frame in data:
+        out.write(frame)
+    out.release()
+
+
+class IntroDetector:
+    def __init__(self, hash_options: dict = None):
+        self.frame_hashes = {}
+        if hash_options is None:
+            hash_options = {}
+        self.hash_options = hash_options
+        self.keyframes = {}
+        self.result = None
+
+    def update(self, frames, name: str):
+        keyframes, frame_indexes = get_key_frames(frames)
+        self.keyframes[name] = []
+        for kf_index, (kf, ts) in enumerate(zip(keyframes, frame_indexes)):
+            h = hash_image(kf, **self.hash_options)
+            if h not in self.frame_hashes:
+                self.frame_hashes[h] = []
+            self.frame_hashes[h].append((name, kf_index))
+            self.keyframes[name].append({'ts': ts, 'kf': kf})
+
+    def detect(self, threshold=0.5, morph=2):
+        shot_repeats_for_intro = max(2, int(len(self.keyframes) * threshold))
+        for frame_hash, occurrences in self.frame_hashes.items():
+            for name, kf_index in occurrences:
+                self.keyframes[name][kf_index]['count'] = len(occurrences)
+        result = {}
+        for name, keyframes in self.keyframes.items():
+            mask = np.array([kf_dict['count'] >= shot_repeats_for_intro for kf_dict in keyframes])
+            if morph > 0:
+                mask = erode(dilate(mask, morph), morph)
+
+            data = self.keyframes[name]
+            for d, m in zip(data, mask):
+                d['intro'] = m
+            # TODO: dont use pandas, for some reason slow af
+            result[name] = pd.DataFrame(data).drop(columns=['kf'])
+        self.result = result
+        return result
+
+    def save(self, save_intros=False):
+        for file_path, df in self.result.items():
+            intro = df.intro.values
+            intro[-1] = 0
+            if not intro.any():
+                continue
+            intro_begins = np.where(intro[1:] & ~intro[:-1])[0] + 1
+            intro_ends = np.where(~intro[1:] & intro[:-1])[0] + 1
+            intro_lengths = intro_ends - intro_begins
+            max_intro_idx = np.argmax(intro_lengths)
+            intro_start = intro_begins[max_intro_idx]
+            intro_end = intro_ends[max_intro_idx]
+            intro_start_frame = int(df.ts[intro_start])
+            intro_end_frame = int(df.ts[intro_end])
+            with open(os.path.splitext(file_path)[0]+'_intro.txt', 'w+') as f:
+                f.writelines([str(intro_start_frame)+'\n', str(intro_end_frame)])
+
+
+
+video_dir = '../static/The Office/season 4'
+paths = [os.path.join(video_dir, video_file) for video_file in os.listdir(video_dir) if video_file.endswith('.mkv')]
+frames = [read_frames(path) for path in paths]
+
+
+detector = IntroDetector()
+for i, f in enumerate(frames):
+    print(i)
+    detector.update(f, paths[i])
+
+res = detector.detect()
+detector.save(save_intros=True)
